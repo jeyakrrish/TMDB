99d5f00199e30c3c9dc824df7ce74247e75976a9
jeyakrrish <jeya.seelan1998@gmail.com>
2022-07-19 11:14:08 +0530

    second commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function App() {
+  return (
+    <div>
+      app compo
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));  //what
+
+root.render(
+  <App />             //where
+);
+
+
